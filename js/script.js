f49ae4a551369e7edd4e78703cadbd8f506cefe2
Steopa2001 <moraru495@gmail.com>
2025-07-04 20:28:32 +0200

    feat: declared the variables

--- js/script.js
+++ js/script.js
@@ -2,9 +2,17 @@
 const listEmail = [
   'jessica@gmail.com',
   'flavio89@gmail.com',
-  'luca456@gmail.com'
-  'giampi-45@gmail.com'
-  'umberto67@gmail.com'
+  'luca456@gmail.com',
+  'giampi-45@gmail.com',
+  'umberto67@gmail.com',
   'moraru495@gmial.com'
 ];
+
+console.log(listEmail);
+
+//dichiaro variaili
+let foundIt = false;
+
+//eseguo prompt per il cliente
+const userEmail  = parseInt(prompt('Digita la tua email'));
 
