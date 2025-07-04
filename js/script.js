75c21b0f1b6ff92133dda2544e86fc6ef5b42dcd
Steopa2001 <moraru495@gmail.com>
2025-07-04 20:53:00 +0200

    feat: achieved the desired result

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ const listEmail = [
   'luca456@gmail.com',
   'giampi-45@gmail.com',
   'umberto67@gmail.com',
-  'moraru495@gmial.com'
+  'moraru495@gmail.com'
 ];
 
 console.log(listEmail);
@@ -14,5 +14,18 @@ console.log(listEmail);
 let foundIt = false;
 
 //eseguo prompt per il cliente
-const userEmail  = parseInt(prompt('Digita la tua email'));
+const userEmail  = prompt('Digita la tua email');
 
+//eseguo il ciclo for 
+for (let i = 0; i < listEmail.length; i++) {
+  if (userEmail === listEmail[i]) {
+    foundIt = true;
+  }
+}
+
+//risultato
+if (foundIt) {
+  console.log("Benvenuto alla festa!");
+} else {
+  console.log("Mi dispiace, non sei nella lista degli invitati.");
+}
